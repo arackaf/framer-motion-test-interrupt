486509ce52b00916d80f69bb8fa5da0179eb5581
Adam Rackis <adamrackis@hotmail.com>
2025-02-13 10:46:18 -0600

    work

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,33 @@ import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [vis, setVis] = useState(true);
 
   return (
-    <div className="flex flex-col">
-      <span>Hello world</span>
-      <span>Yo</span>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <span>Header</span>
+      <button onClick={() => setVis((vis) => !vis)}>Toggle</button>
+      <motion.div
+        style={{ overflow: "hidden" }}
+        animate={{ height: (vis ? 150 : 0) + "px" }}
+        onAnimationEnd={(val) => console.log("End", val)}
+        onAnimationComplete={(val) => console.log("Complete", val)}
+      >
+        Hello World
+        <br />
+        Hello World
+        <br />
+        Hello World
+        <br />
+        Hello World
+        <br />
+        Hello World
+        <br />
+        Hello World
+        <br />
+        Hello World
+        <br />
+      </motion.div>
     </div>
   );
 }
